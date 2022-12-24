c8bfe26ebb9c1af7e22ebdae8e430b399749cb8e
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2022-12-24 23:24:40 +0600

    Launch route fixed and optimized

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -9,11 +9,11 @@ async function httpGetPlanets() {
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // const response = await fetch(`${API_URL}/launches`);
-  // const fetchedLaunches = await response.json();
-  // return fetchedLaunches.sort((a, b) => {
-  //   return a.flightNumber - b.flightNumber;
-  // });
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 // Submit given launch data to launch system.
